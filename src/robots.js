c501a4bd193c144f1c344c03280bc7aae1418a97
binhtd <binhtd.com@gmail.com>
2019-06-29 15:49:12 +0700

    Second Step

--- src/robots.js
+++ src/robots.js
@@ -19,20 +19,44 @@ export const robots = [
     },
     {
         id: 4,
-        name: 'Optimus',
-        username: 'optimuss',
-        email: 'optimuss@gmail.org'
+        name: 'Andree',
+        username: 'andree',
+        email: 'andree@gmail.org'
     },
     {
         id: 5,
-        name: 'Optimus',
-        username: 'optimuss',
-        email: 'optimuss@gmail.com'
+        name: 'Chihuahua',
+        username: 'chihuahua',
+        email: 'chihuahua@gmail.com'
     },
     {
         id: 6,
-        name: 'Optimus',
-        username: 'optimuss',
-        email: 'optimuss@gmail.com'
+        name: 'ComBatLao',
+        username: 'cbl',
+        email: 'combatlao@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Daryss',
+        username: 'daryss',
+        email: 'daryss@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Katy',
+        username: 'katy',
+        email: 'katy@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Police',
+        username: 'police',
+        email: 'police@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Genza',
+        username: 'genza',
+        email: 'genza@gmail.com'
     },
 ]
